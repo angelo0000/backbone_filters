71d2d1b66d91ae3d7180f06d98decf611d6aceb4
Jake Dempsey <angelo0000@gmail.com>
2011-08-12 13:51:00 -0500

    adding plugin and some readme info

---
+++ backbone_filters.js
@@ -0,0 +1,28 @@
+(function() {
+  _.extend(Backbone.Controller.prototype, Backbone.Events, {
+    before: {},
+    after: {},
+		_runFilters: function(filters, fragment, args){
+			_(filters).detect(function(func, filterRoute){
+				if (!_.isRegExp(filterRoute)){
+					filterRoute = this._routeToRegExp(filterRoute);
+				} 
+				if(filterRoute.test(fragment)){
+					return !func.apply(this, args);
+				}
+				return false;
+			}, this);
+		},
+    route : function(route, name, callback) {
+      Backbone.history || (Backbone.history = new Backbone.History);
+      if (!_.isRegExp(route)) route = this._routeToRegExp(route);
+      Backbone.history.route(route, _.bind(function(fragment) {
+        var args = this._extractParameters(route, fragment);
+				this._runFilters(this.before, fragment, args);
+        callback.apply(this, args);
+				this._runFilters(this.after, fragment, args);
+        this.trigger.apply(this, ['route:' + name].concat(args));
+      }, this));
+    }
+  });
+}).call(this);
