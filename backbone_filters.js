3ee1782b0ff46635e01ad10c31f54e4a322f7c95
Jake Dempsey <angelo0000@gmail.com>
2011-08-12 15:22:46 -0500

    fixed a bug where filter chain was being halted when it shouldnt.. will need to clean this up to be a bit more expressive

--- backbone_filters.js
+++ backbone_filters.js
@@ -3,17 +3,21 @@
     before: {},
     after: {},
 		_runFilters: function(filters, fragment, args){
-			_(filters).detect(function(func, filterRoute){
+			if(_(filters).isEmpty()){
+				return true;
+			}
+			return _(filters).detect(function(func, filterRoute){
 				if (!_.isRegExp(filterRoute)){
 					filterRoute = this._routeToRegExp(filterRoute);
 				} 
 				if(filterRoute.test(fragment)){
 					if(_.isFunction(func)){
-						return !func.apply(this, args);
+						var result = func.apply(this, args);
+						return _.isUndefined(result) ? false : !result;
 					}else{
-						return !this[func].apply(this, args);
-					}
-					
+						var result = this[func].apply(this, args);
+						return _.isUndefined(result) ? false : !result;
+					}					
 				}
 				return false;
 			}, this);
@@ -23,10 +27,12 @@
       if (!_.isRegExp(route)) route = this._routeToRegExp(route);
       Backbone.history.route(route, _.bind(function(fragment) {
         var args = this._extractParameters(route, fragment);
-				this._runFilters(this.before, fragment, args);
-        callback.apply(this, args);
-				this._runFilters(this.after, fragment, args);
-        this.trigger.apply(this, ['route:' + name].concat(args));
+				var filterResult = this._runFilters(this.before, fragment, args);
+        if(filterResult == true || _.isUndefined(filterResult)){
+					callback.apply(this, args);
+					this._runFilters(this.after, fragment, args);
+	        this.trigger.apply(this, ['route:' + name].concat(args));					
+				}
       }, this));
     }
   });
