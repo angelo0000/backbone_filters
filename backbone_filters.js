032a7cbb2ac1d35161cb9a5a1f8518a514d8e3a1
Jake Dempsey <angelo0000@gmail.com>
2011-08-12 15:44:16 -0500

    reformat

--- backbone_filters.js
+++ backbone_filters.js
@@ -1,39 +1,41 @@
 (function() {
-  _.extend(Backbone.Controller.prototype, Backbone.Events, {
-    before: {},
-    after: {},
-		_runFilters: function(filters, fragment, args){
-			if(_(filters).isEmpty()){
-				return true;
-			}
-			return _(filters).detect(function(func, filterRoute){
-				if (!_.isRegExp(filterRoute)){
-					filterRoute = this._routeToRegExp(filterRoute);
-				} 
-				if(filterRoute.test(fragment)){
-					if(_.isFunction(func)){
-						var result = func.apply(this, args);
-						return _.isUndefined(result) ? false : !result;
-					}else{
-						var result = this[func].apply(this, args);
-						return _.isUndefined(result) ? false : !result;
-					}					
-				}
-				return false;
-			}, this);
-		},
-    route : function(route, name, callback) {
-      Backbone.history || (Backbone.history = new Backbone.History);
-      if (!_.isRegExp(route)) route = this._routeToRegExp(route);
-      Backbone.history.route(route, _.bind(function(fragment) {
-        var args = this._extractParameters(route, fragment);
-				var filterResult = this._runFilters(this.before, fragment, args);
-        if(filterResult == true || _.isUndefined(filterResult)){
-					callback.apply(this, args);
-					this._runFilters(this.after, fragment, args);
-	        this.trigger.apply(this, ['route:' + name].concat(args));					
-				}
-      }, this));
-    }
-  });
+    _.extend(Backbone.Controller.prototype, Backbone.Events, {
+        before: {},
+        after: {},
+        _runFilters: function(filters, fragment, args) {
+            if (_(filters).isEmpty()) {
+                return true;
+            }
+            return _(filters).detect(function(func, filterRoute) {
+                if (!_.isRegExp(filterRoute)) {
+                    filterRoute = this._routeToRegExp(filterRoute);
+                }
+                if (filterRoute.test(fragment)) {
+                    if (_.isFunction(func)) {
+                        var result = func.apply(this, args);
+                        return _.isUndefined(result) ? false: !result;
+                    } else {
+                        var result = this[func].apply(this, args);
+                        return _.isUndefined(result) ? false: !result;
+                    }
+                }
+                return false;
+            },
+            this);
+        },
+        route: function(route, name, callback) {
+            Backbone.history || (Backbone.history = new Backbone.History);
+            if (!_.isRegExp(route)) route = this._routeToRegExp(route);
+            Backbone.history.route(route, _.bind(function(fragment) {
+                var args = this._extractParameters(route, fragment);
+                var filterResult = this._runFilters(this.before, fragment, args);
+                if (filterResult == true || _.isUndefined(filterResult)) {
+                    callback.apply(this, args);
+                    this._runFilters(this.after, fragment, args);
+                    this.trigger.apply(this, ['route:' + name].concat(args));
+                }
+            },
+            this));
+        }
+    });
 }).call(this);
