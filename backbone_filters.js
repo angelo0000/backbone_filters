806276c0b7179286e70f36f4f5a536805f55ef32
Jake Dempsey <angelo0000@gmail.com>
2011-08-15 09:28:44 -0500

    making run filters only return a true or false so the caller looks a bit cleaner

--- backbone_filters.js
+++ backbone_filters.js
@@ -6,30 +6,26 @@
             if (_(filters).isEmpty()) {
                 return true;
             }
-            return _(filters).detect(function(func, filterRoute) {
+            var failingFilter = _(filters).detect(function(func, filterRoute) {
                 if (!_.isRegExp(filterRoute)) {
                     filterRoute = this._routeToRegExp(filterRoute);
                 }
                 if (filterRoute.test(fragment)) {
-                    if (_.isFunction(func)) {
-                        var result = func.apply(this, args);
-                        return _.isUndefined(result) ? false: !result;
-                    } else {
-                        var result = this[func].apply(this, args);
-                        return _.isUndefined(result) ? false: !result;
-                    }
+									var result = (_.isFunction(func) ? func.apply(this, args) : this[func].apply(this, args));
+									return _.isBoolean(result) && result === false;
                 }
                 return false;
             },
             this);
+						
+						return failingFilter ? false : true;
         },
         route: function(route, name, callback) {
             Backbone.history || (Backbone.history = new Backbone.History);
             if (!_.isRegExp(route)) route = this._routeToRegExp(route);
             Backbone.history.route(route, _.bind(function(fragment) {
                 var args = this._extractParameters(route, fragment);
-                var filterResult = this._runFilters(this.before, fragment, args);
-                if (filterResult == true || _.isUndefined(filterResult)) {
+                if (this._runFilters(this.before, fragment, args)) {
                     callback.apply(this, args);
                     this._runFilters(this.after, fragment, args);
                     this.trigger.apply(this, ['route:' + name].concat(args));
